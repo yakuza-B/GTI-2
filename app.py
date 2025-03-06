282dc17008d9ece06800dcddfd2e70a8c119833a
yakuza-B <barryng1119@gmail.com>
2025-03-06 21:45:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,8 @@ if page == "Introduction":
     st.markdown("<p class='title'>🌍 Global Terrorism Index 2023 Dashboard</p>", unsafe_allow_html=True)
     
     # Center Image
-    st.image(image, use_column_width=True)
+    st.image(image, width=400)
+
     
     # Introduction Text
     st.write("""
